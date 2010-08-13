ed7e286150572fd0d7d9bc62dc2abf6492d329b7
Patrick Stein <Patrick.Stein@tinkerbell.jinx.de>
2010-08-13 16:41:01 +0200

    updated framework to include headers, created JNXFree/JNXFree.h

---
+++ JNXFreeFramework/JNXFree/JNXFree.h
@@ -0,0 +1,26 @@
+/*
+ *  JNXFree.h
+ *  JNXFree
+ *
+ *  Created by Patrick Stein on 10-08-13.
+ *  Copyright 2010 jinx.de. All rights reserved.
+ *
+ */
+
+#import "Default.pch"
+#ifdef __OBJC__
+    #import <Cocoa/Cocoa.h>
+	#import "JNXCrashReporter.h"
+	#import "JNXKeyChainPassword.h"
+	#import "JNXPasswordPanel.h"
+	#import "NSDictionary+KeyValueStringEncoding.h"
+	#import "NSString+PercentEscape.h"
+	#import "JNXMTQueue.h"
+	#import "NSThread+LeopardAdditions.h"
+	#import "NSData+RSAAddition.h"
+	#import "NSData+ZlibAddition.h"
+	#import "NSData+sha1Addition.h"
+#endif
+
+#import "osversion.h"
+
